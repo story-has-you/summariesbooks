f16ed20716242d76a365d3d9357ce1febb6e3e60
fangxi <loveinori@qq.com>
2023-11-23 16:06:23 +0800

    tables

--- app/api/bookList/route.tsx
+++ app/api/bookList/route.tsx
@@ -2,5 +2,8 @@ import { selectList } from '../../models/BookSummary';
 
 export async function GET(request: Request) {
   const bookList = await selectList();
+  if (!bookList) {
+    return Response.json({ data: [] });
+  }
   return Response.json({ data: bookList });
 }
--- tables.sql
+++ tables.sql
@@ -1,14 +1,18 @@
-create table book_summary (
+create table
+  public.book_summary (
     id uuid not null default gen_random_uuid (),
     book_name character varying null default ''::character varying,
     book_author character varying null default ''::character varying,
     publish_date character varying null default ''::character varying,
-    amazon_url text null,
-    cover_url text null,
     summary text null,
     create_time timestamp with time zone null default now(),
     update_time timestamp with time zone null default now(),
+    cover_url text null,
+    gutenberg_url text null,
+    category json null,
+    amazon_url text null,
+    markdown text null,
     constraint book_review_pkey primary key (id)
   ) tablespace pg_default;
 
-
+create index if not exists idx_book_name on public.book_summary using btree (book_name) tablespace pg_default;
