dd14454c20352f6139dac85131d7faccaba115cc
fangxi <loveinori@qq.com>
2023-11-23 10:10:29 +0800

    producthunt

---
+++ app/components/Producthunt/index.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+export default () => {
+  return (
+    <>
+      <section className="flex justify-center">
+        <Link
+          href="https://www.producthunt.com/posts/summariesbooks?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-summariesbooks"
+          target="_blank"
+        >
+          <img
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=426573&theme=light"
+            alt="summariesbooks - Free&#0032;AI&#0045;Powered&#0032;Book&#0032;Summaries | Product Hunt"
+            style={{ width: 250, height: 54 }}
+            width="250"
+            height="54"
+          />
+        </Link>
+      </section>
+    </>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Brand from './components/Brand';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Producthunt from './components/Producthunt';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={`flex flex-col ${inter.className}`}>
         <Header />
         <Brand />
+        <Producthunt />
         {children}
         <Footer />
       </body>
