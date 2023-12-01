b9e92e245bc23b052ed2129cde100c17808a9c28
fangxi <loveinori@qq.com>
2023-12-01 18:00:21 +0800

    代码优化

--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -1,19 +1,17 @@
-import { signInWithEmail, signUpNewUser } from "@/service/auth";
+import { signUpNewUser } from "@/service/auth";
 import { fail, ok } from "@/utils/api";
 import { headers } from "next/headers";
 
 export async function POST(request) {
-  const origin = headers().get("origin");
-  const { email, password, username } = await request.json();
-  const userInfo = initUser(username);
-  const data = await signUpNewUser(
-    email,
-    password,
-    userInfo,
-    `${origin}/auth/callback`
-  );
-  console.log(data);
-  return ok("The verification email has been sent, please check the email");
+  try {
+    const origin = headers().get("origin");
+    const { email, password, username } = await request.json();
+    const userInfo = initUser(username);
+    await signUpNewUser(email, password, userInfo, `${origin}/auth/callback`);
+    return ok();
+  } catch (error) {
+    return fail(error.message);
+  }
 }
 
 const initUser = (username) => {
