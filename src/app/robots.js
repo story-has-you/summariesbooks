325aab87d4b203e24fa96502cb3e3986f4b1d469
fangxi <loveinori@qq.com>
2023-12-08 15:55:51 +0800

    robots.js

---
+++ src/app/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://summariesbooks.com//sitemap.xml',
+  }
+}
