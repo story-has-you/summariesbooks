5017662421c71cde17d52afc1c1f9b1504370d45
fangxi <loveinori@qq.com>
2023-12-08 15:48:53 +0800

    SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,8 @@ import GoogleAnalytics from "@/components/GoogleAnalytics";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "summariesbooks.com",
-  description: "uses GPT-3.5 or GPT-4 to generate a book summary",
+  title: "Summarize books and talk to books",
+  description: "uses GPTs to chat with book",
 };
 
 export default function RootLayout({ children }) {
