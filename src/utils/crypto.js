1e3574b8f4a40a83f1716022e88ab366d240bbc9
fangxi <loveinori@qq.com>
2023-12-12 19:10:51 +0800

    Delete Skeleton component and update crypto.js and tables.sql

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,16 +1,16 @@
 const crypto = require("crypto");
 
 // 定义一个密码短语和盐值
-const passphrase = 'summariesbooks.com';
-const salt = 'summariesbooks'; // 盐值应该是固定的，可以是任何字符串
+const passphrase = process.env.NEXT_PUBLIC_PASSPHRASE;
+const salt = process.env.NEXT_PUBLIC_SALT; // 盐值应该是固定的，可以是任何字符串
 
 // 加密函数 - 箭头函数形式
 export const encrypt = (text) => {
   const key = deriveKey(passphrase, salt, 32);
   const iv = deriveKey(passphrase, salt, 16);
-  const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-  let encrypted = cipher.update(text, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
+  const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
   return encrypted;
 };
 
@@ -18,12 +18,12 @@ export const encrypt = (text) => {
 export const decrypt = (encrypted) => {
   const key = deriveKey(passphrase, salt, 32);
   const iv = deriveKey(passphrase, salt, 16);
-  const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
+  const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
+  let decrypted = decipher.update(encrypted, "hex", "utf8");
+  decrypted += decipher.final("utf8");
   return decrypted;
 };
 
 const deriveKey = (passphrase, salt, keyLength) => {
-  return crypto.pbkdf2Sync(passphrase, salt, 100000, keyLength, 'sha256');
-}
+  return crypto.pbkdf2Sync(passphrase, salt, 100000, keyLength, "sha256");
+};
--- tables.sql
+++ tables.sql
@@ -1,5 +1,16 @@
 create table
-  book_summary (
+  public.book_assistant_relation (
+    assistant_id text null,
+    id uuid not null default gen_random_uuid (),
+    file_id text null,
+    book_id uuid null,
+    constraint book_assistant_relation_pkey primary key (id),
+    constraint book_assistant_relation_book_id_key unique (book_id),
+    constraint book_assistant_relation_book_id_fkey foreign key (book_id) references book_summary (id) on delete cascade
+  ) tablespace pg_default;
+
+  create table
+  public.book_summary (
     id uuid not null default gen_random_uuid (),
     book_name character varying null default ''::character varying,
     book_author character varying null default ''::character varying,
@@ -9,30 +20,23 @@ create table
     update_time timestamp with time zone null default now(),
     cover_url text null,
     gutenberg_url text null,
-    category json null,
+    category json null default '["Text"]'::json,
     amazon_url text null,
-    markdown text null,
     constraint book_review_pkey primary key (id)
   ) tablespace pg_default;
 
 create index if not exists idx_book_name on public.book_summary using btree (book_name) tablespace pg_default;
 
-create table
-  book_assistant_relation (
-    assistant_id text null,
-    id uuid not null default gen_random_uuid (),
-    file_id text null,
-    book_id text null,
-    constraint book_assistant_relation_pkey primary key (id),
-    constraint book_assistant_relation_book_id_key unique (book_id)
-  ) tablespace pg_default;
+create unique index idx_unique_book_name on public.book_summary using btree (book_name) tablespace pg_default;
 
-create table users (
-    id uuid not null references auth.users on delete cascade,
+create table
+  public.users (
+    id uuid not null,
     email character varying(255) not null,
     username character varying(50) not null,
     registration_date timestamp with time zone not null default current_timestamp,
-    openai_key character varying(255) null,
+    openai_key text null,
     constraint users_pkey primary key (id),
-    constraint users_email_key unique (email)
+    constraint users_email_key unique (email),
+    constraint users_id_fkey foreign key (id) references auth.users (id) on delete cascade
   ) tablespace pg_default;
