e6d908e86a8c192c7d219dc0ae9b59de9adc52b9
fangxi <loveinori@qq.com>
2023-12-27 16:07:13 +0800

    nocache

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import { request } from "@/utils/api";
 import Header from "@/components/Header";
 import Brand from "@/components/Brand";
 import Footer from "@/components/Footer";
-import { cache } from "react";
 
 export const metadata = {
   title: "Summarize books and talk to books",
@@ -13,10 +12,10 @@ export const metadata = {
   },
 };
 
-const fetchBooks = cache(async () => {
+const fetchBooks = async () => {
   const { data } = await request("/api/books", { cache: "no-cache" });
   return data;
-});
+};
 
 export default async () => {
   const { books, count } = await fetchBooks();
